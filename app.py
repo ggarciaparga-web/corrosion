7c22d24d6c638750581084c1c62aca1576ba63de
ggarciaparga-web <g.garcia.parga@gmail.com>
2026-04-21 12:42:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -177,6 +177,12 @@ try:
             st.metric("Residual moment", f"{f_sel['Mu (kNm)']:.2f} kNm")
         with col_render:
             st.pyplot(draw_section_2d(inputs_calc, df_cv, year_sel, px0_val))
+        with col_render:
+            st.pyplot(draw_section_2d(inputs_calc, df_cv, year_sel, px0_val))
+            st.image("https://github.com/user-attachments/assets/36960bd8-5f2d-4faf-961e-e340d4b7f6a8", 
+            caption="Referencia de la sección visual",
+                     use_container_width=True
+                    )
 
     with tab4:
         st.header("🧪 Corrosion zone sensitivity")
